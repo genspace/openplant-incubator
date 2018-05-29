964fdd86c14c325282e6e9c9f10f168eae782245
Alex Thomas <alex@areios.io>
2018-05-29 00:23:35 +0000

    Serve index.html

--- raspberry_pi/index.js
+++ raspberry_pi/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+// Serve static files in public directory
+app.use(express.static('public'))
 
 app.listen(9001, () => console.log('Example app listening on port 9001!'))
