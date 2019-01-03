f14dda8006a22bd1dc32b20e8af7c377639a2031
danwchan <danwchan@gmail.com>
2019-01-02 23:41:53 -0500

    use the local version

--- unit_tests/test_lights/test_lights.ino
+++ unit_tests/test_lights/test_lights.ino
@@ -1,8 +1,114 @@
-const int lights = 4;   // pin for the lights
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_TSL2561_U.h>
+
+/* IC2 addr 0x39. If you set the ADDR pin high
+   or low, use TSL2561_ADDR_HIGH (0x49) or TSL2561_ADDR_LOW
+   (0x29) respectively.
+*/
+
+class Lightsensor: public Adafruit_TSL2561_Unified {
+  private:
+    byte readInterval;            // the time in seconds between readings
+
+  public:
+    sensors_event_t event;        // a structure defined in the unified sensor library which contains the measurements
+/*
+ *Constructor, also calls the base class constrcutor
+ *
+ */   
+  Lightsensor(byte seconds) : Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 333) {  // initialize constructor with base class constructor
+    readInterval = seconds;
+  }
+
+  void Update() {
+    unsigned long currentMillis = millis();
+    
+    if((currentMillis - event.timestamp) >= (readInterval * 1000)) {
+      getEvent(&event);
+      }
+    }
+
+  void Configure() {
+    enableAutoRange(true);                                       // set the gain to automatic
+    setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);           // set the medium resolution and speed 
+  }
+
+  uint32_t Readlux() {
+    getEvent(&event);
+    return event.light;
+  }
+
+  void testprint() {
+    unsigned long currentMillis = millis();
+
+    if((currentMillis - event.timestamp) >= (readInterval * 1000)) {
+      Serial.print("Current millis() = "); Serial.println(currentMillis);
+      Serial.print("Millis of last poll = "); Serial.println(event.timestamp);
+      Serial.print("lux = "); Serial.println(event.light);
+      Serial.println();
+    }
+  }
+};
+
+class Ledbar {
+  private:
+    byte outPin;               // the pin which the LED transistor is attached to
+    byte outPwm;               // the PWM output of the pin
+    byte setInterval;          // the time in seconds between checking if the
+    unsigned long lastUpdate;  // to store the millis 
+    bool night;                // if true then the LED should be off
+
+  public:
+    unsigned int setLux;       // the set point of the system in lux
+
+  Ledbar(byte pin, byte seconds, unsigned int set = 200) {
+    night = false;
+    outPin = pin;
+    setLux = set;
+    setInterval = seconds;
+    lastUpdate = millis();
+    outPwm = 120;
+  }
+
+  void Configure() {
+    pinMode(outPin, OUTPUT);
+  }
+
+  void toggle_night() {
+    night = ~night;
+  }
+
+  void Update(Lightsensor sensor) {
+    unsigned long currentMillis = millis();
+    unsigned int measuredLux = sensor.Readlux();
+
+    if((currentMillis - lastUpdate) >= (setInterval * 1000)) {    
+      if (night == false) {
+        while (setLux < measuredLux) {
+          analogWrite(outPin, ++outPwm);
+          measuredLux = sensor.Readlux();
+//          delay(1000);
+          Serial.print(setLux - measuredLux); Serial.println(": going up");
+        }
+        while (setLux > measuredLux) {
+          analogWrite(outPin, --outPwm);
+          measuredLux = sensor.Readlux();
+//          delay(1000);
+          Serial.print(setLux -  measuredLux); Serial.println(": going dowm");
+        }
+      }  
+    }
+  }
+  
+};
+
+
+Lightsensor testsensor = Lightsensor(2);    // initialize the sensor
+Ledbar lights = Ledbar(9, 10, 2000);              // initialize the lights
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(lights, OUTPUT);
+
   pinMode(5, OUTPUT);
   pinMode(3, OUTPUT);
   Serial.begin(9600);
@@ -11,17 +117,15 @@ void setup() {
     delay(10);     // will pause Zero, Leonardo, etc until serial console opens
 
   Serial.println("LED testing begins now");
+  lights.Configure();
+  testsensor.Configure();
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(5, LOW);
-  digitalWrite(3, LOW);
-  Serial.println("lights ON");
-  digitalWrite(lights, HIGH); //for testing purposes
-  delay(5000);
-  digitalWrite(lights, LOW);
-  Serial.println("lights off");
-  delay(5000);
+  testsensor.testprint();
+  testsensor.Update();
+//  digitalWrite(9, 255);
+  Serial.print("lux = "); Serial.println(testsensor.Readlux());
+  lights.Update(testsensor);
 }
--- unit_tests/test_lux/test_lux.ino
+++ unit_tests/test_lux/test_lux.ino
@@ -2,40 +2,51 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_TSL2561_U.h>
 
-/* This driver uses the Adafruit unified sensor library (Adafruit_Sensor),
-   which provides a common 'type' for sensor data and some helper functions.
-   
-   To use this driver you will also need to download the Adafruit_Sensor
-   library and include it in your libraries folder.
-
-   You should also assign a unique ID to this sensor for use with
-   the Adafruit Sensor API so that you can identify this particular
-   sensor in any data logs, etc.  To assign a unique ID, simply
-   provide an appropriate value in the constructor below (12345
-   is used by default in this example).
-   
-   Connections
-   ===========
-   Connect SCL to I2C SCL Clock
-   Connect SDA to I2C SDA Data
-   Connect VDD to 3.3V or 5V (whatever your logic level is)
-   Connect GROUND to common ground
-
-   I2C Address
-   ===========
-   The address will be different depending on whether you leave
-   the ADDR pin floating (addr 0x39), or tie it to ground or vcc. 
-   The default addess is 0x39, which assumes the ADDR pin is floating
-   (not connected to anything).  If you set the ADDR pin high
+/* IC2 addr 0x39. If you set the ADDR pin high
    or low, use TSL2561_ADDR_HIGH (0x49) or TSL2561_ADDR_LOW
    (0x29) respectively.
-    
-   History
-   =======
-   2013/JAN/31  - First version (KTOWN)
 */
-   
-Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
+
+class Lightsensor: public Adafruit_TSL2561_Unified {
+  private:
+    byte readInterval;            // the time in seconds between readings
+
+  public:
+    sensors_event_t event;        // a structure defined in the unified sensor library which contains the measurements
+/*
+ *Constructor, also calls the base class constrcutor
+ *
+ */   
+  Lightsensor(byte seconds) : Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 333) {  // initialize constructor with base class constructor
+    readInterval = seconds;
+  }
+
+  void Update() {
+    unsigned long currentMillis = millis();
+    
+    if((currentMillis - event.timestamp) >= (readInterval * 1000)) {
+      getEvent(&event);
+      }
+    }
+
+  void Configure() {
+    enableAutoRange(true);                                       // set the gain to automatic
+    setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);           // set the medium resolution and speed 
+  }
+
+  void testprint() {
+    unsigned long currentMillis = millis();
+
+    if((currentMillis - event.timestamp) >= (readInterval * 1000)) {
+      Serial.print("Current millis() = "); Serial.println(currentMillis);
+      Serial.print("Millis of last poll = "); Serial.println(event.timestamp);
+      Serial.print("lux = "); Serial.println(event.light);
+      Serial.println();
+    }
+  }
+};
+
+Lightsensor testsensor = Lightsensor(2);  // initialize the sensor
 
 /**************************************************************************/
 /*
@@ -43,10 +54,11 @@ Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 1234
     sensor API sensor_t type (see Adafruit_Sensor for more information)
 */
 /**************************************************************************/
+
 void displaySensorDetails(void)
 {
   sensor_t sensor;
-  tsl.getSensor(&sensor);
+  testsensor.getSensor(&sensor);
   Serial.println("------------------------------------");
   Serial.print  ("Sensor:       "); Serial.println(sensor.name);
   Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
@@ -59,62 +71,37 @@ void displaySensorDetails(void)
   delay(500);
 }
 
-/**************************************************************************/
-/*
-    Configures the gain and integration time for the TSL2561
-*/
-/**************************************************************************/
-void configureSensor(void)
-{
-  /* You can also manually set the gain or enable auto-gain support */
-  // tsl.setGain(TSL2561_GAIN_1X);      /* No gain ... use in bright light to avoid sensor saturation */
-  // tsl.setGain(TSL2561_GAIN_16X);     /* 16x gain ... use in low light to boost sensitivity */
-  tsl.enableAutoRange(true);            /* Auto-gain ... switches automatically between 1x and 16x */
-  
-  /* Changing the integration time gives you better sensor resolution (402ms = 16-bit data) */
-  tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);      /* fast but low resolution */
-  // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);  /* medium resolution and speed   */
-  // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_402MS);  /* 16-bit data but slowest conversions */
-
-  /* Update these values depending on what you've set above! */  
-  Serial.println("------------------------------------");
-  Serial.print  ("Gain:         "); Serial.println("Auto");
-  Serial.print  ("Timing:       "); Serial.println("13 ms");
-  Serial.println("------------------------------------");
-}
 
 /**************************************************************************/
 /*
     Arduino setup function (automatically called at startup)
 */
 /**************************************************************************/
+
 void setup(void) 
 {
+  /* Set the other attached pins so they don't float and cause issues */
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(3, OUTPUT);
   
   Serial.begin(9600);
   Serial.println("Light Sensor Test"); Serial.println("");
+
+  testsensor.Configure();
   
   /* Initialise the sensor */
   //use tsl.begin() to default to Wire, 
   //tsl.begin(&Wire2) directs api to use Wire2, etc.
-  if(!tsl.begin())
+  if(!testsensor.begin())
   {
     /* There was a problem detecting the TSL2561 ... check your connections */
     Serial.print("Ooops, no TSL2561 detected ... Check your wiring or I2C ADDR!");
     while(1);
   }
-  
-  /* Display some basic information on this sensor */
+
   displaySensorDetails();
-  
-  /* Setup the sensor gain and integration time */
-  configureSensor();
-  
-  /* We're ready to go! */
-  Serial.println("");
+
 }
 
 /**************************************************************************/
@@ -125,20 +112,7 @@ void setup(void)
 /**************************************************************************/
 void loop(void) 
 {  
-  /* Get a new sensor event */ 
-  sensors_event_t event;
-  tsl.getEvent(&event);
- 
-  /* Display the results (light is measured in lux) */
-  if (event.light)
-  {
-    Serial.print(event.light); Serial.println(" lux");
-  }
-  else
-  {
-    /* If event.light = 0 lux the sensor is probably saturated
-       and no reliable data could be generated! */
-    Serial.println("Sensor overload");
-  }
-  delay(250);
+  testsensor.testprint();
+  testsensor.Update();
+//  Serial.print("lux = "); Serial.println(testsensor.event.light);
 }
