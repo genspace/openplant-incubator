6b42f31287430eb827e0c0b781162d28627e144d
GTLangseth <grant.langseth@gmail.com>
2019-10-12 15:46:12 -0400

    Create docker file setup

---
+++ v2/models/schema.py
@@ -0,0 +1,64 @@
+#!/usr/env/bin python3
+
+"""
+Main schema file
+"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (
+    Column, ForeignKey, String, Integer, Float, DateTime
+)
+
+Base = declarative_base()
+
+class Plant(Base):
+    __tablename__ = 'plant'
+    id = Column(Integer, primary_key=True)
+    barcode = Column(Integer)
+
+
+class Experiment(Base):
+    __tablename__ = 'experiment'
+    id = Column(Integer, primary_key=True)
+    plant_id = Column(Integer, ForeignKey('plant.id'))
+    vector = Column(String)
+    description = Column(String)
+
+
+class RawImage(Base):
+    __tablename__ = 'raw_image'
+    id = Column(Integer, primary_key=True)
+    incubator_id = Column(Integer, ForeignKey('incubator.id'))
+    time = Column(DateTime)
+    s3_filepath = Column(String)
+
+
+class Image(Base):
+    __tablename__ = 'image'
+    id = Column(Integer, primary_key=True)
+    raw_image_id = Column(Integer, ForeignKey('raw_image.id'))
+    plant_id = Column(Integer, ForeignKey('plant.id'))
+    s3_filepath = Column(String)
+
+
+class Analysis(Base):
+    __tablename__ = 'analysis'
+    id = Column(Integer, primary_key=True)
+    image_id = Column(Integer, ForeignKey('image.id'))
+    size = Column(Float)
+    health = Column(Float)
+
+
+class Sensor(Base):
+    __tablename__ = 'sensor'
+    id = Column(Integer, primary_key=True)
+    incubator_id = Column(Integer, ForeignKey('incubator.id'))
+    time = Column(DateTime)
+    temperature = Column(Float)
+    humidity = Column(Float)
+    light = Column(Integer)
+
+
+class Incubator(Base):
+    __tablename__ = 'incubator'
+    id = Column(Integer, primary_key=True)
