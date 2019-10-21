af81082d07303a202f6ecaea4b94bd8d90a77823
Upom Malik <upom.malik@gmail.com>
2019-10-20 21:16:44 -0400

    added sleep to give database time to intialize

--- v2/setup.py
+++ v2/setup.py
@@ -3,6 +3,8 @@
 import os
 from models import schema
 from sqlalchemy import create_engine
+import time
 
 engine = create_engine(os.environ['CONNECTION_STRING'], echo=True)
+time.sleep(30)
 schema.Base.metadata.create_all(bind=engine)
