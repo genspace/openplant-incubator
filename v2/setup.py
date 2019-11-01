ae6d2def0b7005657a7aa79963b1e1a8924b33cc
GTLangseth <grant.langseth@gmail.com>
2019-11-01 08:59:31 -0400

    Update app setup

--- v2/setup.py
+++ v2/setup.py
@@ -5,11 +5,6 @@ from models import schema
 from sqlalchemy import create_engine
 import time
 
-connection_string = (
-	"mysql+pymysql://root:"
-	os.environ['OPENPLANT_DB_PASSWORD']
-	"@openplant_db/openplant"
-)
-engine = create_engine(connection_string, echo=True)
+engine = create_engine(os.environ['CONNECTION_STRING'], echo=True)
 time.sleep(30)
 schema.Base.metadata.create_all(bind=engine)
