224e2ca9eb01060fd33de9b2566697793a439636
Gordon Fleetwood <gfleetwood@protonmail.com>
2019-11-02 18:36:21 -0400

    Update camera script
    Replaced placeholders with calls to env variables, and added code to delete pictures from pi

--- v2/incubator/camera.py
+++ v2/incubator/camera.py
@@ -6,13 +6,14 @@ import time
 
 def take_picture():
     t = datetime.datetime.now()
-    filename = "{some-folder}" + t.strftime("%Y_%M_%d_%H_%M_%S_%f") + ".jpg"
+    filename = "pictures/" + t.strftime("%Y_%m_%d_%H_%M_%S_%f") + ".jpg"
     print("filename: ", filename)
     with picamera.PiCamera() as camera:
         camera.resolution = (1280,720)
         camera.capture(filename)
     print("picture taken")
-    os.system("{s3-upload}".format(filename))
+    os.system(os.environ["S3UPLOAD"].format(filename))
+    os.system("rm pictures/*")
 
 while True:
     take_picture()
