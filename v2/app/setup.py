84060c87d182280df2b191b9ad10f3707720805f
GTLangseth <grant.langseth@gmail.com>
2019-11-10 02:56:20 -0500

    Update credentials

--- v2/app/setup.py
+++ v2/app/setup.py
@@ -6,7 +6,5 @@ from models import schema
 from sqlalchemy import create_engine
 from util import get_connection_string
 
-
 engine = create_engine(get_connection_string(), echo=True)
-time.sleep(30)
 schema.Base.metadata.create_all(bind=engine)
--- v2/app/util.py
+++ v2/app/util.py
@@ -7,8 +7,9 @@ def get_connection_string():
         os.environ['DB_DRIVER'], '://',
         os.environ['DB_USER'], ':',
         os.environ['DB_PASSWORD'], '@',
-        os.environ['DB_CONTAINER'], '/',
+        os.environ['DB_HOST'], ':3306/',
         os.environ['DB_NAME']
     ]
     connection_string = ''.join(components)
+    print(connection_string)
     return connection_string
