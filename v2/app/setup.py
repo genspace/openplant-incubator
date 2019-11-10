ea1f2ad9b07b5ae08dedc3f2f1611eb38dec4c30
GTLangseth <grant.langseth@gmail.com>
2019-11-10 01:54:35 -0500

    Restructure directory

--- v2/app/setup.py
+++ v2/app/setup.py
@@ -1,10 +1,12 @@
 #!/usr/env/bin python3
 
 import os
+import time
 from models import schema
 from sqlalchemy import create_engine
-import time
+from util import get_connection_string
 
-engine = create_engine(os.environ['CONNECTION_STRING'], echo=True)
+
+engine = create_engine(get_connection_string(), echo=True)
 time.sleep(30)
 schema.Base.metadata.create_all(bind=engine)
