d2ca40e57e878a7e322a5ee92f16686670250108
GTLangseth <grant.langseth@gmail.com>
2019-11-10 16:16:21 -0500

    Add new model

---
+++ v2/app/incubator/bin/ssh.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# password = raspberry
+ssh pi@raspberrypikris.local
---
+++ v2/app/incubator/raspberrypi/log_sensor_data.py
@@ -0,0 +1,94 @@
+#!/usr/env/bin python3
+
+"""
+Script to take data from serial and load
+into the database
+"""
+
+import re
+import os
+import uuid
+import serial
+import logging
+import datetime as dt
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from app.util import get_connection_string
+from app.models.schema import Sensor, Incubator
+
+#-set logger
+logging.basicConfig()
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+#-get incubator id from environment
+incubator_number = os.environ['INCUBATOR_NUMBER']
+
+#-connect to database
+engine = create_engine(get_connection_string())
+session = Session(bind=engine)
+
+#-get incubator id
+incubator_id = session.query()
+
+#-create connection to serial drive
+ser = serial.Serial('/dev/ttyUSB0', 9600)
+
+#-set regex map to parse label input
+regex_map = [
+    ('temperature', r'^Temp.* = ([\d\.]*)$', ),
+    ('humidity', r'^Hum.* = ([\d\.]*)$', ),
+    ('light', r'lights are (\w+)', ),
+]
+
+
+def add_line_to_dict(e, value_dict):
+    """
+    Simple function to parse serial line
+    """
+    for label, regex in regex_map:
+        reg = re.search(e, regex)
+        if reg:
+            val = reg.group(1)
+            value_dict.update({label: val})
+            return value_dict
+    return None
+
+
+def check_dict_completion(value_dict):
+    """
+    Check to see if all values are in record
+    """
+    keys = set(value_dict.keys())
+    labels = set([e[0] for e in regex_map])
+    return len(labels - keys) == 0
+
+
+def upload_record_to_database(value_dict):
+    obj = Sensor()
+    obj.incubator_id = incubator_id
+    obj.time = dt.datetime.today()
+    for k, v in value_dict.items():
+        setattr(obj, k, v)
+    session.add(obj)
+    session.commit()
+    return obj
+
+
+def main():
+    """
+    Parse the lines and add to the database forever
+    """
+    ser.flushInput()
+    value_dict = dict()
+    while 1:
+        if(ser.in_waiting > 0):
+            line = ser.readline().decode("utf-8")
+            add_line_to_dict(line, value_dict)
+        if check_dict_completion(value_dict):
+            upload_record_to_database(value_dict)
+        value_dict = dict()
+    return None
+
+if __name__ == '__main__':
+    main()
