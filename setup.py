9fb7ec1bdc30eb9dd4b78a647aac3a54d212daf6
GTLangseth <grant.langseth@gmail.com>
2021-01-20 19:55:26 -0500

    Bump version number

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ class PostInstallCommand(distutils.cmd.Command):
 
 setuptools.setup(
     name="openplant",
-    version="0.1.03",
+    version="0.1.4",
     author="Genspace",
     description="Open Plant Incubator",
     long_description=long_description,
