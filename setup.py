414137adce68e177874d9286d3ccbf5d272aa806
Grant Langseth <grant.langseth@gmail.com>
2021-03-01 21:07:19 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ class PostInstallCommand(distutils.cmd.Command):
 
 setuptools.setup(
     name="openplant",
-    version="0.1.5",
+    version="0.1.6",
     author="Genspace",
     description="Open Plant Incubator",
     long_description=long_description,
